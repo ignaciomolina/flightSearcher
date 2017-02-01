aabd3d41e7d50eda5fc71979f188b2574d31d307
Ignacio Molina Cuquerella <neldan@gmail.com>
2017-02-01 21:35:35 +0100

    Add Airline pojo class with tests

---
+++ src/main/java/com/ignaciomolina/flightsearcher/pojo/Airline.java
@@ -0,0 +1,75 @@
+package com.ignaciomolina.flightsearcher.pojo;
+
+import java.util.Objects;
+
+public class Airline {
+
+    private String code;
+    private String name;
+    private float infantPrice;
+
+    public Airline(String code, String name, float infantPrice) {
+
+        this.code = Objects.requireNonNull(code, "Code cannot be null.");
+        this.name = Objects.requireNonNull(name, "Name cannot be null.");
+        this.infantPrice = infantPrice;
+    }
+
+    public String getCode() {
+
+        return code;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public float getInfantPrice() {
+
+        return infantPrice;
+    }
+
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + code.hashCode();
+        result = prime * result + Float.floatToIntBits(infantPrice);
+        result = prime * result + name.hashCode();
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+
+            return true;
+        }
+
+        if (obj == null) {
+
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+
+            return false;
+        }
+
+        Airline other = (Airline) obj;
+
+        if (!code.equals(other.code) ||
+            !name.equals(other.name) ||
+            infantPrice != other.infantPrice) {
+
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/com/ignaciomolina/flightsearcher/pojo/AirlineTest.java
@@ -0,0 +1,100 @@
+package com.ignaciomolina.flightsearcher.pojo;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.assertj.core.api.BDDSoftAssertions;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AirlineTest {
+
+    private static final float INFANT_PRICE = 18;
+    private static final String NAME = "Iberia";
+    private static final String CODE = "IB";
+
+    private Airline airline;
+
+    @Before
+    public void setup() {
+
+        airline = new Airline(CODE, NAME, INFANT_PRICE);
+    }
+
+    @Test
+    public void shouldCreateValidAirline() {
+
+        BDDSoftAssertions softly = new BDDSoftAssertions();
+
+        softly.then(airline.getCode()).as("Code").isEqualTo(CODE);
+        softly.then(airline.getName()).as("Name").isEqualTo(NAME);
+        softly.then(airline.getInfantPrice()).as("Infant Price").isEqualTo(INFANT_PRICE);
+
+        softly.assertAll();
+    }
+
+    private void checkEquality(Airline airline, Airline other, boolean areEquals) {
+
+        BDDSoftAssertions softly = new BDDSoftAssertions();
+
+        then(airline.equals(other)).as("Equal method").isEqualTo(areEquals);
+        then(airline.hashCode() == other.hashCode()).as("hashCode method").isEqualTo(areEquals);
+
+        softly.assertAll();
+
+    }
+
+    @Test
+    public void shouldAirlineBeEqualToItself() {
+
+        checkEquality(airline, airline, true);
+    }
+
+    @Test
+    public void shouldAirlineBeEqualToOtherAirlineWithSameFields() {
+
+        Airline other = new Airline(CODE, NAME, INFANT_PRICE);
+
+        checkEquality(airline, other, true);
+    }
+
+    @Test
+    public void shouldAirlineBeNotEqualToOtherAirlineWithDifferentCode() {
+
+        Airline other = new Airline("XX", NAME, INFANT_PRICE);
+
+        checkEquality(airline, other, false);
+    }
+
+    @Test
+    public void shouldAirlineBeNotEqualToOtherAirlineWithDifferentNamee() {
+
+        Airline other = new Airline(CODE, "MyAirline", INFANT_PRICE);
+
+        checkEquality(airline, other, false);
+    }
+
+    @Test
+    public void shouldAirlineBeNotEqualToOtherAirlineWithDifferentInfantPrice() {
+
+        Airline other = new Airline(CODE, NAME, 100.0F);
+
+        checkEquality(airline, other, false);
+    }
+
+    @Test
+    public void shouldAirlineBeNotEqualToNull() {
+
+        then(airline).isNotEqualTo(null);
+    }
+    @Test(expected = NullPointerException.class)
+    public void shouldNotAcceptNullCode() {
+
+        airline = new Airline(null, NAME, INFANT_PRICE);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldNotAcceptNullName() {
+
+        airline = new Airline(CODE, null, INFANT_PRICE);
+    }
+}
